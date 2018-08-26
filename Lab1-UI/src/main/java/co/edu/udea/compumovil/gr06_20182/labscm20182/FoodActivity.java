e99a28cce0d754fbef459c534fa8f995c0c2008c
carloshincapie <solrac.hincapie@gmail.com>
2018-08-25 22:47:50 -0500

    Terminada las dos pantallas portraint y pickup y selección de imagen de galería

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20182/labscm20182/FoodActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20182/labscm20182/FoodActivity.java
@@ -1,13 +1,73 @@
 package co.edu.udea.compumovil.gr06_20182.labscm20182;
 
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
-public class FoodActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class FoodActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+    ImageView imageView;
+    Uri imageUri;
+    private TextView mTimeDisplay;
+    private int mHour;
+    private int mMinute;
+
+    private static int RESULT_LOAD_IMG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+        imageView = (ImageView) findViewById(R.id.imgView);
+        mTimeDisplay = (TextView) findViewById(R.id.textViewTime);
+
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        updateDisplay();
+    }
+
+    private void updateDisplay() {
+        mTimeDisplay.setText(new StringBuilder()
+                .append(mHour).append(":").append(mMinute));
+    }
+
+    public void loadImagefromGallery(View view) {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+    }
+
+    public void onClickPicker(View view) {
+        DialogFragment timePickerFragment = new TimePickerFragment();
+        timePickerFragment.show(getFragmentManager(), "timePicker");
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode == RESULT_OK && requestCode == RESULT_LOAD_IMG){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+
+        }
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        mHour = hourOfDay;
+        mMinute = minute;
+        updateDisplay();
     }
 }
