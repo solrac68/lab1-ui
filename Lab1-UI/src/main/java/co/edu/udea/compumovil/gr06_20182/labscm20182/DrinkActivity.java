7bff20c707cf507da6e50ee000ef34d743234ab4
carloshincapie <solrac.hincapie@gmail.com>
2018-08-26 22:04:44 -0500

    Guarda el estado, falta que almacene la foto

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20182/labscm20182/DrinkActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20182/labscm20182/DrinkActivity.java
@@ -1,6 +1,8 @@
 package co.edu.udea.compumovil.gr06_20182.labscm20182;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,7 +13,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.net.URL;
+import java.io.File;
+import java.net.URI;
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -21,6 +24,14 @@ public class DrinkActivity extends AppCompatActivity {
     private EditText txtPrecio;
     private EditText txtIngredient;
     private TextView txtView;
+
+    SharedPreferences sharedpreferences;
+    public static final String mypreference = "mypref";
+    public static final String Name = "nameKey";
+    public static final String Precio = "precio";
+    public static final String Ingredient = "ingredient";
+    public static final String ImageUri = "imageUri";
+
 
     private static int RESULT_LOAD_IMG = 1;
     @Override
@@ -32,21 +43,73 @@ public class DrinkActivity extends AppCompatActivity {
         txtName = (EditText)findViewById(R.id.txtName);
         txtPrecio = (EditText)findViewById(R.id.txtPrecio);
         txtIngredient = (EditText)findViewById(R.id.txtIngredient);
+
+        sharedpreferences = getSharedPreferences(mypreference,
+                Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(Name)) {
+            txtName.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Precio)) {
+            txtPrecio.setText(sharedpreferences.getString(Precio, ""));
+        }
+        if (sharedpreferences.contains(Ingredient)) {
+            txtIngredient.setText(sharedpreferences.getString(Ingredient, ""));
+        }
+
+        if (sharedpreferences.contains(ImageUri)) {
+            imageUri = Uri.fromFile(new File(sharedpreferences.getString(ImageUri, "")));
+            imageView.setImageURI(imageUri);
+        }
+
+        loadView();
+
+    }
+
+    private void loadView() {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(getResources().getString(R.string.str_Name))
+                .append(":")
+                .append(sharedpreferences.getString(Name, ""))
+                .append("\n")
+                .append(getResources().getString(R.string.str_price))
+                .append(":")
+                .append(sharedpreferences.getString(Precio, ""))
+                .append("\n")
+                .append(getResources().getString(R.string.str_ingredients))
+                .append(":")
+                .append(sharedpreferences.getString(Ingredient, ""));
+
+        txtView.setText(sb);
     }
 
     public void loadImagefromGallery(View view) {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(Name, txtName.getText().toString());
+        editor.putString(Precio, txtPrecio.getText().toString());
+        editor.putString(Ingredient, txtIngredient.getText().toString());
+        editor.apply();
+        loadView();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
+        if (sharedpreferences.contains(ImageUri)) {
+            imageUri = Uri.fromFile(new File(sharedpreferences.getString(ImageUri, "")));
+            imageView.setImageURI(imageUri);
+        }
         if(resultCode == RESULT_OK && requestCode == RESULT_LOAD_IMG){
             imageUri = data.getData();
             imageView.setImageURI(imageUri);
 
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putString(ImageUri,imageUri.toString());
+            editor.apply();
         }
     }
 
@@ -56,12 +119,8 @@ public class DrinkActivity extends AppCompatActivity {
         return true;
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        UIHelper.killApp(true);
-    }
     private void exitApplication() {
-        onDestroy();
+        System.exit(0);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20182/labscm20182/FoodActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr06_20182/labscm20182/FoodActivity.java
@@ -2,10 +2,13 @@ package co.edu.udea.compumovil.gr06_20182.labscm20182;
 
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +17,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.RadioButton;
 
 import java.util.Calendar;
 
@@ -25,15 +29,28 @@ public class FoodActivity extends AppCompatActivity implements TimePickerDialog.
     private CheckBox checkBoxMorning;
     private CheckBox checkBoxAfternoon;
     private CheckBox checkBoxNight;
+    private RadioButton radioButtonPrincipalFood;
+    private RadioButton radioButtonEntry;
     private EditText txtPrecio;
     private EditText txtIngredient;
     private TextView txtView;
-    private TextView textViewTime;
+    //private TextView textViewTime;
     private EditText txtName;
-
-
     private int mHour;
     private int mMinute;
+
+    SharedPreferences sharedpreferences;
+    public static final String mypreference = "mypref2";
+    public static final String CheckBoxMorning = "checkBoxMorning";
+    public static final String CheckBoxAfternoon = "checkBoxAfternoon";
+    public static final String CheckBoxNight = "checkBoxNight";
+    public static final String RadioButtonPrincipalFood = "radioButtonPrincipalFood";
+    public static final String RadioButtonEntry = "radioButtonEntry";
+    public static final String Precio = "precio";
+    public static final String Ingredient = "ingredient";
+    public static final String ImageUri = "imageUri";
+    public static final String TimeDisplay = "mTimeDisplay";
+    public static final String Name = "nameKey";
 
     private static int RESULT_LOAD_IMG = 1;
 
@@ -47,6 +64,8 @@ public class FoodActivity extends AppCompatActivity implements TimePickerDialog.
         checkBoxMorning = (CheckBox)findViewById(R.id.checkBoxMorning);
         checkBoxAfternoon = (CheckBox)findViewById(R.id.checkBoxAfternoon);
         checkBoxNight = (CheckBox)findViewById(R.id.checkBoxNight);
+        radioButtonPrincipalFood = (RadioButton) findViewById(R.id.radioButtonPrincipalFood);
+        radioButtonEntry = (RadioButton) findViewById(R.id.radioButtonEntry);
         txtPrecio =  (EditText)findViewById(R.id.txtPrecio);
         txtIngredient = (EditText)findViewById(R.id.txtIngredient);
         txtView = (TextView) findViewById(R.id.txtView);
@@ -56,7 +75,108 @@ public class FoodActivity extends AppCompatActivity implements TimePickerDialog.
         mHour = c.get(Calendar.HOUR_OF_DAY);
         mMinute = c.get(Calendar.MINUTE);
 
-        updateDisplay();
+        sharedpreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
+        if (sharedpreferences.contains(Name)) {
+            txtName.setText(sharedpreferences.getString(Name, ""));
+        }
+        if (sharedpreferences.contains(Precio)) {
+            txtPrecio.setText(sharedpreferences.getString(Precio, ""));
+        }
+        if (sharedpreferences.contains(CheckBoxMorning)) {
+            Boolean valorCheckMorning = sharedpreferences.getBoolean(CheckBoxMorning, false);
+            checkBoxMorning.setChecked(valorCheckMorning);
+        }
+        if (sharedpreferences.contains(CheckBoxAfternoon)) {
+            Boolean valorCheckAfternoon = sharedpreferences.getBoolean(CheckBoxAfternoon, false);
+            checkBoxAfternoon.setChecked(valorCheckAfternoon);
+        }
+        if (sharedpreferences.contains(CheckBoxNight)) {
+            Boolean valorCheckNight = sharedpreferences.getBoolean(CheckBoxNight, false);
+            checkBoxNight.setChecked(valorCheckNight);
+        }
+        if (sharedpreferences.contains(RadioButtonPrincipalFood)) {
+            Boolean valorCheck = sharedpreferences.getBoolean(RadioButtonPrincipalFood, true);
+            radioButtonPrincipalFood.setChecked(valorCheck);
+        }
+        if (sharedpreferences.contains(RadioButtonEntry)) {
+            Boolean valorCheck = sharedpreferences.getBoolean(RadioButtonEntry, false);
+            radioButtonEntry.setChecked(valorCheck);
+        }
+        if (sharedpreferences.contains(Ingredient)) {
+            txtIngredient.setText(sharedpreferences.getString(Ingredient, ""));
+        }
+        if (sharedpreferences.contains(TimeDisplay)) {
+            String hour = new StringBuilder().append(mHour).append(":").append(mMinute).toString();
+            mTimeDisplay.setText(sharedpreferences.getString(TimeDisplay, hour));
+        }
+
+        //updateDisplay();
+        loadView();
+    }
+
+    private void loadView() {
+
+        Boolean morning = sharedpreferences.getBoolean(CheckBoxMorning, false);
+        Boolean night = sharedpreferences.getBoolean(CheckBoxNight, false);
+        Boolean afternoon = sharedpreferences.getBoolean(CheckBoxAfternoon, false);
+        StringBuilder sbHora = new StringBuilder();
+        if(morning){
+            sbHora.append(getResources().getString(R.string.str_morning));
+            sbHora.append(",");
+        }
+        if(night){
+            sbHora.append(getResources().getString(R.string.str_night));
+            sbHora.append(",");
+        }
+        if(afternoon){
+            sbHora.append(getResources().getString(R.string.str_afternoon));
+            sbHora.append(",");
+        }
+
+
+        if(sbHora.length() == 0) {
+            sbHora.append("");
+        }
+
+
+        Boolean principalfood = sharedpreferences.getBoolean(RadioButtonPrincipalFood, true);
+        String food;
+        if(principalfood){
+            food = getResources().getString(R.string.str_platofuerte);
+        }
+        else{
+            food = getResources().getString(R.string.str_entrada);
+        }
+
+        Log.d("loadView",food);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(getResources().getString(R.string.str_Name))
+                .append(":")
+                .append(sharedpreferences.getString(Name, ""))
+                .append("\n")
+                .append(getResources().getString(R.string.str_date))
+                .append(":")
+                .append(sbHora)
+                .append("\n")
+                .append(getResources().getString(R.string.str_type))
+                .append(":")
+                .append(food)
+                .append("\n")
+                .append(getResources().getString(R.string.str_hour))
+                .append(":")
+                .append(sharedpreferences.getString(TimeDisplay, ""))
+                .append("\n")
+                .append(getResources().getString(R.string.str_price))
+                .append(":")
+                .append(sharedpreferences.getString(Precio, ""))
+                .append("\n")
+                .append(getResources().getString(R.string.str_ingredients))
+                .append(":")
+                .append(sharedpreferences.getString(Ingredient, ""));
+
+
+        txtView.setText(sb);
     }
 
     private void updateDisplay() {
@@ -68,6 +188,23 @@ public class FoodActivity extends AppCompatActivity implements TimePickerDialog.
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(Name, txtName.getText().toString());
+        editor.putString(Precio, txtPrecio.getText().toString());
+        editor.putString(Ingredient, txtIngredient.getText().toString());
+        editor.putString(TimeDisplay, mTimeDisplay.getText().toString());
+        editor.putBoolean(CheckBoxMorning, checkBoxMorning.isChecked());
+        editor.putBoolean(CheckBoxAfternoon, checkBoxAfternoon.isChecked());
+        editor.putBoolean(CheckBoxNight, checkBoxNight.isChecked());
+        editor.putBoolean(RadioButtonEntry, radioButtonEntry.isChecked());
+        editor.putBoolean(RadioButtonPrincipalFood, radioButtonPrincipalFood.isChecked());
+
+        editor.apply();
+
+        Log.d("loadImagefromGallery",radioButtonPrincipalFood.toString());
+
+        loadView();
     }
 
     public void onClickPicker(View view) {
@@ -82,7 +219,6 @@ public class FoodActivity extends AppCompatActivity implements TimePickerDialog.
         if(resultCode == RESULT_OK && requestCode == RESULT_LOAD_IMG){
             imageUri = data.getData();
             imageView.setImageURI(imageUri);
-
         }
     }
 
@@ -121,7 +257,7 @@ public class FoodActivity extends AppCompatActivity implements TimePickerDialog.
     }
 
     private void exitApplication() {
-        onDestroy();
+        System.exit(0);
     }
 
     @Override
@@ -130,8 +266,4 @@ public class FoodActivity extends AppCompatActivity implements TimePickerDialog.
         return true;
     }
 
-    public void onDestroy() {
-        super.onDestroy();
-        UIHelper.killApp(true);
-    }
 }
