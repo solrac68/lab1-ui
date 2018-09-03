59f219625b893ebdef94c998ca2b6bd0efcdfc4e
carloshincapie <solrac.hincapie@gmail.com>
2018-09-02 19:00:33 -0500

    Pantallas de registro y logeo

---
+++ lab2activities/src/main/java/co/edu/udea/compumovil/gr06_20182/lab2/RegisterActivity.java
@@ -0,0 +1,27 @@
+package co.edu.udea.compumovil.gr06_20182.lab2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set View to register.xml
+        setContentView(R.layout.register);
+
+        TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+
+        // Listening to Login Screen link
+        loginScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Closing registration screen
+                // Switching to Login Screen/closing register screen
+                finish();
+            }
+        });
+    }
+}
